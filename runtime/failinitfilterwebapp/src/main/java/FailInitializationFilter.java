421d1cc65e81071e6a98a0a5c67caf9acfef7562
Ludovic Champenois <ludo@google.com>
2022-05-10 16:20:34 -0700

    Move servlet filter init failure test to open source.
    PiperOrigin-RevId: 447853756
    Change-Id: I9d6ba66c23c72c53c68b5d22a1f8147ecb1d35d2

---
+++ runtime/failinitfilterwebapp/src/main/java/FailInitializationFilter.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class FailInitializationFilter implements Filter {
+  @Override
+  public void init(FilterConfig config) throws ServletException {
+    throw new ServletException("Intentionally failing to initialize.");
+  }
+
+  @Override
+  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+      throws ServletException {
+    throw new ServletException("Unexpectedly got a request.");
+  }
+
+  @Override
+  public void destroy() {}
+}
