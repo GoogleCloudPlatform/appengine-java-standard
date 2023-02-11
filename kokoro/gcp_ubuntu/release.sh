a095a4fb7f235c29f671a9f73cc0e33c64880227
Ludovic Champenois <ludo@google.com>
2023-02-11 02:53:21 -0800

    Internal change
    PiperOrigin-RevId: 508863455
    Change-Id: Ie157fa8227784d6ce3b6c206f3580f7e7a7908a4

--- kokoro/gcp_ubuntu/release.sh
+++ kokoro/gcp_ubuntu/release.sh
@@ -95,6 +95,9 @@ echo "Using release number="$RELEASE_NUMBER
 # Work in a $RELEASE_NUMBER branch, not main.
 git checkout -b $RELEASE_NUMBER
 
+git config user.email gae-java-bot@google.com
+git config user.name gae-java-bot
+
 # Make sure `JAVA_HOME` is set.
 echo "JAVA_HOME = $JAVA_HOME"
 
@@ -105,8 +108,6 @@ sudo apt-get -qq update && sudo apt-get -qq install -y maven
 echo "Calling release:prepare and release:perform."
 mvn release:prepare release:perform -B -q --settings=../settings.xml -DskipTests -Darguments=-DskipTests -Dgpg.homedir=${GNUPGHOME} -Dgpg.passphrase=${GPG_PASSPHRASE}
 
-git config user.email gae-java-bot@google.com
-git config user.name gae-java-bot
 git remote set-url origin https://gae-java-bot:${GAE_JAVA_BOT_GITHUB_TOKEN}@github.com/GoogleCloudPlatform/appengine-java-standard
 echo "Doing git tag and push."
 git tag -a v$RELEASE_NUMBER -m v$RELEASE_NUMBER
