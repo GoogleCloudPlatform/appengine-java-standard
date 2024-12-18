61fedeb93c23df5b6092bff80a13d0759f040eed
srinjoyray <srinjoyray@google.com>
2024-12-18 11:14:04 +0530

    addressed review comments

--- runtime/test/src/test/java/com/google/apphosting/runtime/jetty9/SendErrorTest.java
+++ runtime/test/src/test/java/com/google/apphosting/runtime/jetty9/SendErrorTest.java
@@ -83,22 +83,22 @@ public class SendErrorTest extends JavaRuntimeViaHttpBase {
     String url = runtime.jettyUrl("/send-error");
     ContentResponse response = httpClient.GET(url);
     assertEquals(HttpStatus.OK_200, response.getStatus());
-    assertThat(response.getContentAsString(), containsString("<h1>Hello, world!</h1>"));
+    assertThat(response.getContentAsString(), containsString("<h1>Hello, welcome to App Engine Java Standard!</h1>"));
 
     url = runtime.jettyUrl("/send-error?errorCode=404");
     response = httpClient.GET(url);
     assertEquals(HttpStatus.NOT_FOUND_404, response.getStatus());
-    assertThat(response.getContentAsString(), containsString("<h1>Error 404 Not Found</h1>"));
+    assertThat(response.getContentAsString(), containsString("<h1>404 - Page Not Found (App Engine Java Standard)</h1>"));
 
     url = runtime.jettyUrl("/send-error?errorCode=500");
     response = httpClient.GET(url);
     assertEquals(HttpStatus.INTERNAL_SERVER_ERROR_500, response.getStatus());
-    assertThat(response.getContentAsString(), containsString("<h1>Error 500 Internal Server Error</h1>"));
+    assertThat(response.getContentAsString(), containsString("<h1>500 - Internal Server Error (App Engine Java Standard)</h1>"));
 
     url = runtime.jettyUrl("/send-error?errorCode=503");
     response = httpClient.GET(url);
     assertEquals(HttpStatus.SERVICE_UNAVAILABLE_503, response.getStatus());
-    assertThat(response.getContentAsString(), containsString("<h1>Unhandled Error</h1>"));
+    assertThat(response.getContentAsString(), containsString("<h1>Unhandled Error - Service Temporarily Unavailable (App Engine Java Standard)</h1>"));
 
   }
 
