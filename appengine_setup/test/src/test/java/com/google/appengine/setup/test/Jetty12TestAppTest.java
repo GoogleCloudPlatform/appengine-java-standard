9496f9c6d7b5d54a9ee7dadca7ec88e1467571c2
Ludovic Champenois <ludo@google.com>
2025-06-21 06:53:39 -0700

    Upgrade GAE Java version from 2.0.36 to 2.0.37 and prepare next version 2.0.38-SNAPSHOT
    PiperOrigin-RevId: 774105162
    Change-Id: I3928b1f8de649985aaea414c9e87274507ebaf94

--- appengine_setup/test/src/test/java/com/google/appengine/setup/test/Jetty12TestAppTest.java
+++ appengine_setup/test/src/test/java/com/google/appengine/setup/test/Jetty12TestAppTest.java
@@ -26,6 +26,6 @@ public class Jetty12TestAppTest extends TestAppBase {
     @Override
     protected String relativePathForUserApplicationJar() {
     return "../testapps/jetty12_testapp/target/"
-        + "jetty12_testapp-2.0.37-SNAPSHOT-jar-with-dependencies.jar";
+        + "jetty12_testapp-2.0.38-SNAPSHOT-jar-with-dependencies.jar";
     }
 }
