52c2eb632929a4320c50c030467b37715b500348
GAE Java Team <gae-java-bot@google.com>
2026-09-15 05:56:07 -0700

    No public description
    PiperOrigin-RevId: 981769556
    Change-Id: I253af5f8538931e25ac493985b87d9701a8bb8de

--- api_dev/src/main/java/com/google/appengine/tools/info/SdkFactory.java
+++ api_dev/src/main/java/com/google/appengine/tools/info/SdkFactory.java
@@ -34,8 +34,6 @@ final class SdkFactory {
     } else if (useEE11) {
       currentSdk = new Jetty121EE11Sdk();
     } else {
-      // We keep Jetty9 legacy SDK for internal testing only.
-      // For external usage, we default to Jetty12.1 EE8 SDK.
       currentSdk = new Jetty121EE8Sdk();
     }
     return currentSdk;
